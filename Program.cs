aaa6a6fb83f486f7f896ab1b3f18dad7da215151
Ryan <Ryan.Bee.Dev@outlook.com>
2025-02-10 17:48:31 -0500

    Swagger + Index
    Add post build event to generate swagger file + Add Index.html to host API spec on Github Pages

--- Program.cs
+++ Program.cs
@@ -103,7 +103,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 //Enable CORS to allow anything from dev host
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000"));
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "http://localhost:4173"));
 
 app.UseAuthentication();
 app.UseAuthorization();
